186eac17cad23e2684bf5cd81365959c45265eea
lunarifish <messenger@lunariio.work>
2024-03-15 08:28:12 -0400

    feat: 给skider_control节点的启动脚本增加了配置文件路径检测

--- src/skider_control/launch/skider_control.launch.py
+++ src/skider_control/launch/skider_control.launch.py
@@ -1,10 +1,19 @@
 import os
+import sys
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
+SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]               # 获取这个脚本所在目录
+PACKAGE_DIR = os.path.join(SCRIPT_DIR, os.pardir)                       # 向上两级，到skider_control/
+PARAM_CONFIG_PATH = os.path.join(PACKAGE_DIR, 'config', 'params.yaml')  # params.yaml的路径
 
+def generate_launch_description():
+    # 检查参数配置文件是否存在，不存在就退出
+    if not os.path.exists(PARAM_CONFIG_PATH):
+        print("[ERROR][skider_control.launch.py]: config/params.yaml does not exist")
+        sys.exit(1)
+        
     return LaunchDescription(
         [
             Node(
@@ -12,8 +21,7 @@ def generate_launch_description():
                 executable='gimbal_demo_node',
                 name='gimbal_demo_node',
                 output='screen',
-                # parameters=[os.path.join(get_package_share_directory('skider_control'), 'config', 'params.yaml')],
-                parameters=["/home/nuc/Desktop/control/src/skider_control/config/params.yaml"],
+                parameters=[PARAM_CONFIG_PATH],
                 
             ),
             Node(
@@ -21,8 +29,7 @@ def generate_launch_description():
                 executable='chassis_demo_node',
                 name='chassis_demo_node',
                 output='screen',
-                # parameters=[os.path.join(get_package_share_directory('skider_control'), 'config', 'params.yaml')],
-                parameters=["/home/nuc/Desktop/control/src/skider_control/config/params.yaml"],
+                parameters=[PARAM_CONFIG_PATH],
                 
             ),
         ]
